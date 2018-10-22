e177d040ad6d791591e24c7b2e41e8879f64c8af
bhagya-kurthkoti <bhagyashri.k@softvision.com>
2018-10-22 16:04:06 +0530

    test

--- GitSample/src/main/java/sample/HelloWorld.java
+++ GitSample/src/main/java/sample/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     //This is my assessment first commit
+     System.out.println("Hello World");
 	}
 
 }
